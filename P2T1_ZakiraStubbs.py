7b576de3e2ce7122bd8479a13475a8b6d1a71f8c
stubbsz7158 <43038829+stubbsz7158@users.noreply.github.com>
2018-09-06 10:15:54 -0400

    Add files via upload

---
+++ P2T1_ZakiraStubbs.py
@@ -0,0 +1,15 @@
+# A brief description of the project
+# Date
+# CTI-110 P2T1 - Sales Prediction
+# Zakira Stubbs
+#
+
+#Get the projected total sales.
+total_sales = float(input('Enter the projected sales:'))
+
+# Calculate the profit as 23 precent of total sales.
+profit= total_sales * 0.23
+
+# Displat the profit.
+print('The profit is $', format(profit, ',.2f'))
+    
